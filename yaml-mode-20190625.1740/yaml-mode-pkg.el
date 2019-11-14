438ee8520045bf2bf5c49dba9a9c33d4dd4f0b1f
Terje Sannum <terje.sannum@nav.no>
2019-11-14 07:32:16 +0100

    initial commit

---
+++ yaml-mode-20190625.1740/yaml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yaml-mode" "20190625.1740" "Major mode for editing YAML files" '((emacs "24.1")) :commit "0aceaf3888dc484d1f5246d5a9462a4780844081" :keywords '("data" "yaml") :authors '(("Yoshiki Kurihara" . "clouder@gmail.com") ("Marshall T. Vandegrift" . "llasram@gmail.com")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@gmail.com"))
