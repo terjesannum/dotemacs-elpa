8635181b198abd20cc461164411709fb53e43761
Terje Sannum <terje.sannum@nav.no>
2020-02-20 08:08:55 +0100

    upgrade

--- kubectx-mode-20191219.1337/kubectx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kubectx-mode" "20191219.1337" "Change kubectl context/namespace and show in mode line" '((emacs "24")) :authors '(("Terje Sannum" . "terje@offpiste.org")) :maintainer '("Terje Sannum" . "terje@offpiste.org") :url "https://github.com/terjesannum/emacs-kubectx-mode")
---
+++ kubectx-mode-20200116.1918/kubectx-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kubectx-mode" "20200116.1918" "Change kubectl context/namespace and show in mode line" '((emacs "24")) :commit "f08687ae5403eb18bbeffc6dafdfde469bdb9a36" :keywords '("tools" "kubernetes") :authors '(("Terje Sannum" . "terje@offpiste.org")) :maintainer '("Terje Sannum" . "terje@offpiste.org") :url "https://github.com/terjesannum/emacs-kubectx-mode")
