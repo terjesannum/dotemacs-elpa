32bea97f0a3dc2adc57595526c7552c3b552ad05
Terje Sannum <terje.sannum@nav.no>
2020-08-28 10:16:14 +0200

    upgrade

---
+++ logstash-conf-20200725.1843/logstash-conf-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/s145839/.emacs.d/elpa/logstash-conf-20200725.1843/logstash-conf.el  -*- no-byte-compile: t -*-
+(define-package "logstash-conf" "20200725.1843" "basic mode for editing logstash configuration" 'nil :commit "131565042f8f12b9b88bd477959246dd034fa7d6" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
