e9faa91e3c4848b02a5655a6a104a18021dda5fb
Terje Sannum <terje.sannum@nav.no>
2020-11-13 11:28:30 +0100

    upgrade

--- circe-20200928.2345/circe-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "circe" "20200928.2345" "Client for IRC in Emacs"
-  '((cl-lib "0.5"))
-  :commit "d98986ce933c380b47d727beea8bad81bda65dc9" :url "https://github.com/jorgenschaefer/circe")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
