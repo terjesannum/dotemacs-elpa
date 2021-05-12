65a3c5a6c2b41241545ec79cdc433ca5e20d6aed
Terje Sannum <terje.sannum@nav.no>
2021-05-12 12:05:39 +0200

    upgrade

---
+++ circe-20210508.1616/circe-pkg.el
@@ -0,0 +1,13 @@
+(define-package "circe" "20210508.1616" "Client for IRC in Emacs"
+  '((emacs "24.4")
+    (cl-lib "0.5"))
+  :commit "2798a75d625677402b01f138ebac6eb53337d9d6" :authors
+  '(("Jorgen Schaefer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Jorgen Schaefer" . "forcer@forcix.cx")
+  :keywords
+  '("irc" "chat" "comm")
+  :url "https://github.com/jorgenschaefer/circe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ git-commit-20210328.1730/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20210328.1730" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "d8a06bb2e27f4970140b1e5cd6965a52d0bb1e1c" :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
