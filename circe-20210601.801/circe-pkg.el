5f686db5a1690f89de17648c68cfce54cb54badb
Terje Sannum <terje.sannum@nav.no>
2021-06-03 11:02:07 +0200

    upgrade

---
+++ circe-20210601.801/circe-pkg.el
@@ -0,0 +1,13 @@
+(define-package "circe" "20210601.801" "Client for IRC in Emacs"
+  '((emacs "24.4")
+    (cl-lib "0.5"))
+  :commit "c0b2f997b3b73640d635ee84627bb8cf36c9adfe" :authors
+  '(("Jorgen Schaefer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Jorgen Schaefer" . "forcer@forcix.cx")
+  :keywords
+  '("irc" "chat" "comm")
+  :url "https://github.com/jorgenschaefer/circe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
