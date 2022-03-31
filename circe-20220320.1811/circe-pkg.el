ee04bef2edce5f325e82e8740a724e066b6b66f7
Terje Sannum <terje.sannum@nav.no>
2022-03-31 07:56:17 +0200

    upgrade

---
+++ circe-20220320.1811/circe-pkg.el
@@ -0,0 +1,13 @@
+(define-package "circe" "20220320.1811" "Client for IRC in Emacs"
+  '((emacs "24.4")
+    (cl-lib "0.5"))
+  :commit "e4c5bb69e6f1338239bdbf66baf57bb610828b2e" :authors
+  '(("Jorgen Schaefer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Jorgen Schaefer" . "forcer@forcix.cx")
+  :keywords
+  '("irc" "chat" "comm")
+  :url "https://github.com/emacs-circe/circe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ git-commit-20220328.47/git-commit-pkg.el
@@ -0,0 +1,17 @@
+(define-package "git-commit" "20220328.47" "Edit Git commit messages."
+  '((emacs "25.1")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "d1ad778280f651f0ab87ce191784b96b3ebe312a" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Sebastian Wiesner" . "lunaryorn@gmail.com")
+    ("Florian Ragwitz" . "rafl@debian.org")
+    ("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
