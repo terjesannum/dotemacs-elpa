aed917d39bb168bf4c4f4133c4134b20182c181d
Terje Sannum <terje.sannum@nav.no>
2022-08-15 19:42:10 +0200

    upgrade

--- helm-3.8.6/helm-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from helm.el  -*- no-byte-compile: t -*-
-(define-package "helm" "3.8.6" "Helm is an Emacs incremental and narrowing framework" '((helm-core "3.8.4") (popup "0.5.3")) :commit "0fc13ddb13b1c8e70eb6c0c7f7d46308a55f2344" :authors '(("Thierry Volpiatto ")) :maintainer '("Thierry Volpiatto ") :url "https://emacs-helm.github.io/helm/")
