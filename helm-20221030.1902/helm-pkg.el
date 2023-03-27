6123dbf27ceb24864a0bfbdfbe15ca9f74a315ba
Terje Sannum <terje.sannum@nav.no>
2023-03-27 22:16:33 +0200

    Revert "upgrade"
    This reverts commit 693aec54cb2d641b643bd9d17e9c1fa30772c4b3.

---
+++ helm-20221030.1902/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20221030.1902" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.8")
+    (popup "0.5.3"))
+  :commit "edfd77e51dcda1fd81a0bf45685ba3f560ddc327" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
