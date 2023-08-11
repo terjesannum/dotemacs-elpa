7983b309f906e6c95355936c4f69ba64db20eef2
Terje Sannum <terje.sannum@nav.no>
2023-08-11 15:53:20 +0200

    upgrade

---
+++ helm-20230811.1011/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20230811.1011" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.3")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "5afd7a438a179f0e9031816a223872b6024b6d29" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
