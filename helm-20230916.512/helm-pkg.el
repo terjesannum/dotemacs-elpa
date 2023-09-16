a77aa19be005cb764882337bd2dd341b13ce794d
Terje Sannum <terje.sannum@nav.no>
2023-09-16 17:44:44 +0200

    upgrade

---
+++ helm-20230916.512/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20230916.512" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.4")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "fddf8e32aa4ff865f2ed725c3ab9e226837f5541" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ helm-core-20230916.652/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20230916.652" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "9514cfb55daa3fd3928b0730a6d85e181fc19fb5" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
