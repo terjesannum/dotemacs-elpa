64949848f3055eac81115bec5302bb07165c128b
Terje Sannum <terje.sannum@nav.no>
2023-09-18 07:50:57 +0200

    new package

---
+++ helm-comint-20230911.1725/helm-comint-pkg.el
@@ -0,0 +1,15 @@
+(define-package "helm-comint" "20230911.1725" "Comint prompt navigation for helm"
+  '((emacs "29.1")
+    (helm "3.9.4"))
+  :commit "60476860d88b9786118ffb9a593f8006779a4c55" :authors
+  '(("Pierre Neidhardt" . "mail@ambrevar.xyz"))
+  :maintainers
+  '(("Benedict Wang" . "foss@bhw.name"))
+  :maintainer
+  '("Benedict Wang" . "foss@bhw.name")
+  :keywords
+  '("processes" "matching")
+  :url "https://github.com/benedicthw/helm-comint.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
