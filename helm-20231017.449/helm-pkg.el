0637c974a0b92a1a9eb15a0ee7f8a537fd6efdca
Terje Sannum <terje.sannum@nav.no>
2023-10-20 13:57:07 +0200

    upgrade

---
+++ helm-20231017.449/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20231017.449" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.4")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "372167f13f496242263c7014a555e40db12627fb" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ lsp-mode-20231018.1322/lsp-mode-pkg.el
@@ -0,0 +1,21 @@
+(define-package "lsp-mode" "20231018.1322" "LSP mode"
+  '((emacs "27.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "a1e86f68781471f1dbec39b539b2b5cb734980f2" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainers
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
