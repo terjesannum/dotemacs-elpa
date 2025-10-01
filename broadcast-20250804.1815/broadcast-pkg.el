5bcd0adda7a46d7badaea6df6fbafc08ff67deb4
Terje Sannum <terje.sannum@nav.no>
2025-10-01 15:14:40 +0200

    upgrade

---
+++ broadcast-20250804.1815/broadcast-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "broadcast" "20250804.1815"
+  "Links buffers together for simultaneous editing."
+  '((emacs "24.4"))
+  :url "https://github.com/killdash9/broadcast.el"
+  :commit "c6fff897c14f0aac9c44a7d8ca3f70782e4c38d0"
+  :revdesc "c6fff897c14f"
+  :keywords '("convenience" "frames" "link" "cursors")
+  :authors '(("Russell Black" . "(killdash9@github)"))
+  :maintainers '(("Russell Black" . "(killdash9@github)")))
