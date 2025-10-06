61bf8ed0ffd9c2cd01a41857ed538715feb33962
Terje Sannum <terje.sannum@nav.no>
2025-10-06 12:58:16 +0200

    add gptel

---
+++ gptel-20251006.320/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20251006.320"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "9128ccde033df1662e8f412f1f2c830b07fc63c7"
+  :revdesc "9128ccde033d"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
---
+++ transient-20251005.1943/transient-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20251005.1943"
+  "Transient commands."
+  '((emacs  "26.1")
+    (compat "30.1")
+    (seq    "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "6ed97c26add376e3d00d363ab12ece93f0835ef0"
+  :revdesc "6ed97c26add3"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
