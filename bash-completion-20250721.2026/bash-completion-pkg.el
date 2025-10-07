dca109cb161106938541082c42693fa656d21e40
Terje Sannum <terje.sannum@nav.no>
2025-10-07 16:35:37 +0200

    add bash-completion

---
+++ bash-completion-20250721.2026/bash-completion-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "bash-completion" "20250721.2026"
+  "Bash completion for the shell buffer."
+  '((emacs "25.3"))
+  :url "http://github.com/szermatt/emacs-bash-completion"
+  :commit "762f28fefba487e15d626691310f3194804eb71a"
+  :revdesc "762f28fefba4"
+  :keywords '("convenience" "unix")
+  :authors '(("Stephane Zermatten" . "szermatt@gmx.net"))
+  :maintainers '(("Stephane Zermatten" . "szermatt@gmail.com")))
