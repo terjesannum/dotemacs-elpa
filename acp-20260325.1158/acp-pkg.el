8ca874141f8e8fd7c8569a503e030ad36693cb72
Terje Sannum <terje.sannum@nav.no>
2026-04-29 13:09:35 +0200

    add agent-shell

---
+++ acp-20260325.1158/acp-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "acp" "20260325.1158"
+  "An ACP (Agent Client Protocol) implementation."
+  '((emacs "28.1"))
+  :url "https://github.com/xenodium/acp.el"
+  :commit "c32fbf8df34ed0095853a8cf55dc783e68b67d90"
+  :revdesc "c32fbf8df34e")
---
+++ agent-shell-20260428.1807/agent-shell-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "agent-shell" "20260428.1807"
+  "Native agentic integrations for Claude Code, Gemini CLI, etc."
+  '((emacs       "29.1")
+    (shell-maker "0.90.1")
+    (acp         "0.11.1"))
+  :url "https://github.com/xenodium/agent-shell"
+  :commit "9368925f40f92c6de79cf8c1f3d95265e56da213"
+  :revdesc "9368925f40f9")
