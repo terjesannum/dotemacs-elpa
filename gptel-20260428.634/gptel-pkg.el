fdcde0b770a2914ab4dc93c33d30086e2af0ff12
Terje Sannum <terje.sannum@nav.no>
2026-04-29 12:48:57 +0200

    upgrade

---
+++ gptel-20260428.634/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20260428.634"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "71b9f9414536c4cd35d8c02d3c8ad4fc4aea8718"
+  :revdesc "71b9f9414536"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
---
+++ lsp-mode-20260428.1058/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20260428.1058"
+  "LSP mode."
+  '((emacs         "28.1")
+    (dash          "2.18.0")
+    (f             "0.21.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "f6c60b6df14487340af924ed0f59ed08f1c8da6b"
+  :revdesc "f6c60b6df144"
+  :keywords '("languages"))
